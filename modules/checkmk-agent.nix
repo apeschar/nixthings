216fccf4ff2d77cdae1e208009e8ff9e8b3aae53
Albert Peschar <albert@peschar.net>
2017-06-13 08:16:22 +0200

    Add RabbitMQ agent for Check_MK

--- modules/checkmk-agent.nix
+++ modules/checkmk-agent.nix
@@ -2,7 +2,7 @@
 
 let
 
-  inherit (lib) mkIf mkOption types;
+  inherit (lib) mkIf mkOption types concatStrings mapAttrsToList;
   inherit (pkgs) stdenv fetchurl;
   inherit (pkgs.stdenv) mkDerivation;
 
@@ -26,46 +26,83 @@ let
     '';
   };
 
-  mysqlPlugin = mkDerivation {
-    name = "checkmk-agent-mysql";
-    version = "1.4.0p2";
+  plugins = {
+    mysql = mkDerivation {
+      name = "checkmk-agent-mysql";
+      version = "1.4.0p2";
 
-    src = fetchurl {
-      url = "https://mon.kibo.li/kibo/check_mk/agents/plugins/mk_mysql";
-      sha256 = "00w0x8pylwwidbs5s0nl4knxx6rgds67k3w1h3q314x8hwxzx9m5";
+      src = fetchurl {
+        url = "https://mon.kibo.li/kibo/check_mk/agents/plugins/mk_mysql";
+        sha256 = "00w0x8pylwwidbs5s0nl4knxx6rgds67k3w1h3q314x8hwxzx9m5";
+      };
+
+      builder = builtins.toFile "builder.sh" ''
+        source $stdenv/setup
+        mkdir -p $out/bin
+        cp $src $out/bin/mk_mysql
+        sed -i 's#--defaults-extra-file=\$MK_CONFDIR/mysql.cfg##g' $out/bin/mk_mysql
+        chmod +x $out/bin/mk_mysql
+        patchShebangs $out/bin/mk_mysql
+      '';
     };
 
-    builder = builtins.toFile "builder.sh" ''
-      source $stdenv/setup
-      mkdir -p $out/bin
-      cp $src $out/bin/mk_mysql
-      sed -i 's#--defaults-extra-file=\$MK_CONFDIR/mysql.cfg##g' $out/bin/mk_mysql
-      chmod +x $out/bin/mk_mysql
-      patchShebangs $out/bin/mk_mysql
-    '';
+    rabbitmq = mkDerivation {
+      name = "checkmk-agent-rabbitmq";
+      version = "8123feb";
+
+      src = fetchurl {
+        url = "https://raw.githubusercontent.com/disconn3ct/cmk-rabbitmq/8123feb166b2ac72bd7939a34adc6c824c10e4d6/share/check_mk/agents/plugins/rabbitmq.py";
+        sha256 = "13gpj2jh2j5yc4kiy290aa0jnxyc21pnl44lpaxfn5c15j2ccjw1";
+      };
+
+      buildInputs = [ pkgs.python ];
+
+      builder = builtins.toFile "builder.sh" ''
+        source $stdenv/setup
+        mkdir -p $out/bin
+        cp $src $out/bin/mk_rabbitmq
+        chmod +x $out/bin/mk_rabbitmq
+        patchShebangs $out/bin/mk_rabbitmq
+      '';
+    };
   };
 
-  agentWithPlugins = mkDerivation {
+  buildAgent = agent: plugins: configFiles: mkDerivation {
     name = "checkmk-agent-full";
 
-    inherit agent mysqlPlugin;
+    inherit agent;
+    inherit plugins;
 
-    builder = builtins.toFile "builder.sh" ''
+    builder = pkgs.writeScript "builder.sh" ''
       source $stdenv/setup
 
-      mkdir -p $out/bin $out/lib/check_mk/plugins
+      mkdir -p \
+        $out/bin \
+        $out/lib/check_mk/plugins \
+        $out/etc/check_mk
 
-      ln -s $mysqlPlugin/bin/mk_mysql $out/lib/check_mk/plugins/mk_mysql
+      for plugin in $plugins; do
+        for f in $plugin/bin/*; do
+          ln -s $f $out/lib/check_mk/plugins/''${f##*/}
+        done
+      done
+
+      ${concatStrings (mapAttrsToList (filename: content: ''
+        cp ${pkgs.writeText filename content} $out/etc/check_mk/'${filename}'
+      '') configFiles)}
 
       cat > $out/bin/check_mk_agent <<EOF
       #! $shell
       export MK_LIBDIR="$out/lib/check_mk"
-      exec $agent/bin/check_mk_agent "$@"
+      export MK_CONFDIR="$out/etc/check_mk"
+      exec $agent/bin/check_mk_agent "\$@"
       EOF
 
       chmod +x $out/bin/check_mk_agent
     '';
   };
+
+  configuredAgent = buildAgent cfg.agent cfg.plugins cfg.configFiles;
 
   sshKey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDcOPSnFqxA0krADDHhuNyNEcmO4GE2Jf/mYqKucKu4KRCeEAeFxcpQWs+EE0TvVdBB8kbNN17vxOSPWEUBlrF/zoRov7ySEwmzz4+BY62L8Hv4PK1a4UfC6EC1I7AmExCeYD1aCToMckm8DIIibUAGV7OPX/VwkgGjjpHVGxZB0ZIcs6LoewlRi9y/BqtY97ImIEO1Jo4Y0Gc+gAy1CUdOL6L91dkAhU68W/cnVYYCnWzfmxmpjR90mKcgmgaux3I84uOb8/xd/gue9SUUq7x1X43WKPI5TD+oVRdXxaNXs8e9OT4H0iC1dpvxtDRIijrdcim2i0ILa1II1yb96sHX3BxcWsytwIZNSud8cBsaYUb9ZbHCwnw+SHxK0ErZ2lESGrM4XclgJ0Ph9Rkg/B8L+doyEEmWOHrGYknVsl7rAo53O2NhzvByIoy6CSnoQ4e/eMqox3XJLkImthf0yvC80K8w5+aTdhCDFO+DLxcl1xGPUG/2o6eymlzTnaVoHes0UC7RzZAQWTVR0geBA89lGoAp1bQ4bL+EUJITB6x40DHhjM3IZjqn3yXjNbgkJTVcWF09CZwDpGTfMfb2pif/SsNjTRYCzkVYdaIKV4+onb9rjpmzkfgscfcOIQA4kFQmVG8+V9rKDXLFv9Lcf8S7RGNyDfdk3d+ZV8//KXSjnw== kibo@mon.kibo.li";
 
@@ -82,14 +119,35 @@ in
 
     agent = mkOption {
       type = types.package;
-      default = agentWithPlugins;
+      default = agent;
+    };
+
+    plugins = mkOption {
+      type = types.listOf types.package;
+      default = with plugins; [ mysql rabbitmq ];
+    };
+
+    configFiles = mkOption {
+      type = types.attrsOf types.str;
+      default = {
+        "rabbitmq.cfg" = ''
+          servers = [
+            {
+              'address':  '127.0.0.1',
+              'port':     15672,
+              'user':     'guest',
+              'password': 'guest',
+            },
+          ]
+        '';
+      };
     };
 
   };
 
   config = mkIf cfg.enable {
 
-    environment.systemPackages = [ cfg.agent ];
+    environment.systemPackages = [ configuredAgent ];
 
     users.users.root.openssh.authorizedKeys.keys = [
       ''command="${agent}/bin/check_mk_agent",no-port-forwarding,no-x11-forwarding,no-agent-forwarding ${sshKey}''
