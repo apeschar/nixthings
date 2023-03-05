247c106498d983fa801b699689d488d21490b0d6
Albert Peschar <albert@peschar.net>
2023-03-05 11:07:24 +0100

    Enable SMART monitoring in Check_MK agent

--- modules/checkmk.nix
+++ modules/checkmk.nix
@@ -6,19 +6,17 @@
 }:
 with lib; let
   cfg = config.kibo.checkmk;
+  check_mk_agent = pkgs.check_mk_agent.override (_: {
+    enablePluginSmart = true;
+  });
 in {
   options.kibo.checkmk = {
     enable = mkEnableOption "Check_MK agent";
   };
 
   config = mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [
-      check_mk_agent
-      smartmontools
-    ];
-
     users.users.root.openssh.authorizedKeys.keys = [
-      ''restrict,command="check_mk_agent || true" ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEJN9YVPInBiZh9TBnGPR053R/RWNI+gBGCikwBtB/pS''
+      ''restrict,command="${check_mk_agent}/bin/check_mk_agent || true" ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEJN9YVPInBiZh9TBnGPR053R/RWNI+gBGCikwBtB/pS''
     ];
   };
 }
