db9170943dac47dbfc47eb3781127ddf1c2b38e7
Albert Peschar <albert@peschar.net>
2023-06-06 12:16:34 +0300

    Add ipgen.ip6

--- flake.nix
+++ flake.nix
@@ -8,9 +8,9 @@
   };
 
   outputs = {
+    self,
     nixpkgs,
     checkmk,
-    ...
   }: {
     nixosModule = {
       imports = [
@@ -23,6 +23,29 @@
     lib = {
       inherit ((import ./lib/net.nix {inherit (nixpkgs) lib;}).lib) net;
       math = import ./lib/math.nix;
+      ipgen = import ./lib/ipgen.nix {
+        inherit (self) lib;
+        inherit nixpkgs;
+      };
+    };
+
+    tests = nixpkgs.lib.runTests {
+      testPow = {
+        expr = self.lib.math.pow 2 8;
+        expected = 256;
+      };
+      testIpgen64 = let
+        subnet = "2a01:4f9:3051:429c::/64";
+      in {
+        expr = self.lib.ipgen.ip6 subnet "hello";
+        expected = "2a01:4f9:3051:429c:26e8:3b2a:c5b9:e29e";
+      };
+      testIpgen32 = let
+        subnet = "2a01:4f9:3051:429c::/32";
+      in {
+        expr = self.lib.ipgen.ip6 subnet "hello";
+        expected = "2a01:4f9:5fb0:a30e:26e8:3b2a:c5b9:e29e";
+      };
     };
   };
 }
---
+++ lib/ipgen.nix
@@ -0,0 +1,15 @@
+{
+  lib,
+  nixpkgs,
+}: let
+  hextet = hex: i: builtins.substring (4 * i) 4 hex;
+  addressOfHex = hex: nixpkgs.lib.concatMapStringsSep ":" (hextet hex) (nixpkgs.lib.range 0 7);
+  addressOfValue = value: addressOfHex (builtins.hashString "sha256" value);
+in {
+  ip6 = subnet: value: let
+    prefixLength = lib.net.cidr.length subnet;
+    addr = addressOfValue value;
+    host = lib.net.ip.subtract (lib.net.cidr.host 0 (lib.net.cidr.make prefixLength addr)) addr;
+  in
+    lib.net.cidr.host host subnet;
+}
