322ad2e0f9cf14bdf5dbaf9f4f4ce23814df2f60
Albert Peschar <albert@peschar.net>
2023-06-06 16:19:08 +0300

    Revert "dd ipgen.ip6ll"
    This reverts commit e61b7280731f9616f76845c57565376aefdb5184.

--- flake.nix
+++ flake.nix
@@ -46,10 +46,6 @@
         expr = self.lib.ipgen.ip6 subnet "hello";
         expected = "2a01:4f9:5fb0:a30e:26e8:3b2a:c5b9:e29e";
       };
-      testIpgenLL = {
-        expr = self.lib.ipgen.ip6ll "hello";
-        expected = "fe80:4dba:5fb0:a30e:26e8:3b2a:c5b9:e29e";
-      };
     };
   };
 }
