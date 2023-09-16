db5fe26e7b6699397d655ab36e8b7c874a723cab
Albert Peschar <albert@peschar.net>
2023-09-16 12:53:54 +0000

    Fix compatibility with old nixpkgs

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -1,8 +1,16 @@
-{lib, ...}: {
-  services.openssh.settings.AcceptEnv = lib.concatStringsSep " " [
-    "GIT_AUTHOR_NAME"
-    "GIT_AUTHOR_EMAIL"
-    "GIT_COMMITTER_NAME"
-    "GIT_COMMITTER_EMAIL"
-  ];
+{
+  lib,
+  options,
+  ...
+}: {
+  services.openssh = lib.optionalAttrs (builtins.hasAttr "settings" options.services.openssh) {
+    settings = {
+      AcceptEnv = lib.concatStringsSep " " [
+        "GIT_AUTHOR_NAME"
+        "GIT_AUTHOR_EMAIL"
+        "GIT_COMMITTER_NAME"
+        "GIT_COMMITTER_EMAIL"
+      ];
+    };
+  };
 }
