dd7af9f0b3a512ec770df693b831123ef3c1bf4c
Albert Peschar <albert@peschar.net>
2023-12-03 12:09:07 +0000

    Add update-authorized-keys script

--- flake.nix
+++ flake.nix
@@ -11,7 +11,10 @@
     self,
     nixpkgs,
     checkmk,
-  }: {
+  }: let
+    inherit (nixpkgs) lib;
+    eachSystem = lib.genAttrs (builtins.attrNames nixpkgs.legacyPackages);
+  in {
     nixosModule = {
       imports = [
         {nixpkgs.overlays = [checkmk.overlays.default];}
@@ -39,5 +42,58 @@
       inherit (self.lib) math ipgen;
       inherit (nixpkgs.lib) runTests;
     };
+
+    packages = eachSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      update-authorized-keys = pkgs.writeScriptBin "update-authorized-keys" ''
+        #!${pkgs.python3}/bin/python
+
+        import sys
+        from argparse import ArgumentParser
+        from pathlib import Path
+
+        parser = ArgumentParser()
+        parser.add_argument('file')
+        parser.add_argument('--dry-run', '-n', action='store_true')
+
+        KEYS = ${builtins.toJSON self.lib.sshKeys.albert}
+
+        def main():
+          args = parser.parse_args()
+          file = Path(args.file)
+
+          try:
+            contents = file.read_text().rstrip()
+          except FileNotFoundError:
+            log("Creating %s", file)
+            contents = ""
+
+          lines = [] if contents == "" else contents.split('\n')
+
+          add_lines = ['%s update-authorized-keys' % k for k in KEYS]
+
+          lines = [l for l in lines if not l.endswith(' update-authorized-keys') or l in add_lines]
+
+          for line in add_lines:
+            if line not in lines:
+              lines.append(line)
+
+          contents = '\n'.join(lines) + '\n'
+
+          if args.dry_run:
+            print(contents, end="")
+          else:
+            file.write_text(contents)
+
+          return 0
+
+        def log(fmt, *args):
+          print(fmt % args, file=sys.stderr)
+
+        if __name__ == '__main__':
+          sys.exit(main())
+      '';
+    });
   };
 }
