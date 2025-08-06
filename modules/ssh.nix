f5d1ea4f1322f4fb22d599149befd89bfeb288af
Albert Peschar <albert@peschar.net>
2025-08-06 14:15:25 +0000

    ssh: enable OpenSSH, disable password auth

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -14,16 +14,22 @@
     '';
   };
 in {
-  services.openssh = lib.optionalAttrs (builtins.hasAttr "settings" options.services.openssh) {
-    settings = {
-      AcceptEnv = lib.concatStringsSep " " [
-        "GIT_AUTHOR_NAME"
-        "GIT_AUTHOR_EMAIL"
-        "GIT_COMMITTER_NAME"
-        "GIT_COMMITTER_EMAIL"
-      ];
+  services.openssh =
+    {
+      enable = true;
+    }
+    // lib.optionalAttrs (builtins.hasAttr "settings" options.services.openssh) {
+      settings = {
+        PasswordAuthentication = false;
+        KbdInteractiveAuthentication = false;
+        AcceptEnv = lib.concatStringsSep " " [
+          "GIT_AUTHOR_NAME"
+          "GIT_AUTHOR_EMAIL"
+          "GIT_COMMITTER_NAME"
+          "GIT_COMMITTER_EMAIL"
+        ];
+      };
     };
-  };
 
   programs.ssh.knownHostsFiles = [githubKnownHosts];
 }
