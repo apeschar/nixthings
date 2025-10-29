e518978d99a70f92b711e990551f0e2e6933f071
Albert Peschar <albert@peschar.net>
2025-10-29 14:23:07 +0000

    checkmk: add support for job monitoring

--- modules/checkmk.nix
+++ modules/checkmk.nix
@@ -2,21 +2,54 @@
   lib,
   config,
   pkgs,
+  utils,
   ...
-}:
-with lib; let
+}: let
   cfg = config.kibo.checkmk;
   check_mk_agent = pkgs.check_mk_agent.override (_: {
     enablePluginSmart = true;
   });
+  userOpts = _: {
+    options = {
+      enableCheckmkJobs = lib.mkOption {
+        type = lib.types.bool;
+        default = false;
+      };
+    };
+  };
 in {
-  options.kibo.checkmk = {
-    enable = mkEnableOption "Check_MK agent";
+  options = {
+    kibo.checkmk = {
+      enable = lib.mkEnableOption "Check_MK agent";
+    };
+
+    users.users = lib.mkOption {
+      type = lib.types.attrsOf (lib.types.submodule userOpts);
+    };
   };
 
-  config = mkIf cfg.enable {
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = [check_mk_agent];
+
     users.users.root.openssh.authorizedKeys.keys = [
       ''restrict,command="${check_mk_agent}/bin/check_mk_agent || true" ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINO8C6W9yu7jokdtA5RWJ5n4pD7zpfoOa/zz1KXz3F8C''
     ];
+
+    systemd.services = lib.mapAttrs' (_: {name, ...}: {
+      name = "checkmk-jobs-${name}";
+      value = {
+        wantedBy = ["multi-user.target"];
+        serviceConfig = {
+          Type = "oneshot";
+          ExecStart = let
+            dir = "/var/lib/check_mk_agent/job/${name}";
+          in [
+            ["${pkgs.coreutils}/bin/mkdir" "-p" dir]
+            ["${pkgs.coreutils}/bin/chmod" "700" dir]
+            ["${pkgs.coreutils}/bin/chown" "--" name dir]
+          ];
+        };
+      };
+    }) (lib.filterAttrs (_: u: u.enableCheckmkJobs) config.users.users);
   };
 }
