f0c99d5fe2bf169e309c93009202972f13eeac50
Albert Peschar <albert@peschar.net>
2026-05-20 07:41:11 +0000

    Trigger postgresql-certs via ACME postRun

--- modules/postgresql.nix
+++ modules/postgresql.nix
@@ -32,63 +32,70 @@ in {
 
   config = let
     ifCert = lib.mkIf (cfg.certName != null);
-  in {
-    systemd.services = {
-      postgresql-certs = ifCert {
-        wants = ["acme-finished-${cfg.certName}.target"];
-        after = ["acme-selfsigned-${cfg.certName}.service" "acme-finished-${cfg.certName}.target"];
-        wantedBy = ["acme-selfsigned-${cfg.certName}.service" "acme-finished-${cfg.certName}.target"];
-        serviceConfig = {
-          Type = "oneshot";
-          ExecStart = copyCerts;
+  in
+    lib.mkMerge [
+      (lib.mkIf (cfg.certName != null) {
+        security.acme.certs.${cfg.certName}.postRun = lib.mkAfter ''
+          ${pkgs.systemd}/bin/systemctl start --no-block postgresql-certs.service
+        '';
+      })
+      {
+        systemd.services = {
+          postgresql-certs = ifCert {
+            wants = ["acme-${cfg.certName}.service"];
+            after = ["acme-${cfg.certName}.service"];
+            serviceConfig = {
+              Type = "oneshot";
+              ExecStart = copyCerts;
+            };
+          };
+
+          postgresql = ifCert {
+            wants = ["postgresql-certs.service"];
+            after = ["postgresql-certs.service"];
+          };
+
+          postgresql-upgrade = lib.mkIf (cfg.nextPackage != null && cfg.nextPackage.psqlSchema > config.services.postgresql.package.psqlSchema) {
+            conflicts = ["postgresql.service"];
+            wants = ["postgresql-certs.service"];
+            after = ["postgresql-certs.service"];
+            serviceConfig = {
+              Type = "oneshot";
+              RuntimeDirectory = "postgresql";
+              User = "postgres";
+              Group = "postgres";
+              ExecStart = pkgs.writeShellScript "postgresql-upgrade" ''
+                set -euxo pipefail
+
+                NEWDATA="/var/lib/postgresql/${cfg.nextPackage.psqlSchema}"
+                NEWBIN="${cfg.nextPackage}/bin"
+                OLDDATA="${config.services.postgresql.dataDir}"
+                OLDBIN="${config.services.postgresql.package}/bin"
+
+                rm -rf "$NEWDATA"
+
+                install -d -m 0700 -o postgres -g postgres "$NEWDATA"
+
+                cd "$NEWDATA"
+
+                "$NEWBIN/initdb" --no-sync --no-data-checksums -D "$NEWDATA"
+
+                "$NEWBIN/pg_upgrade" \
+                  --old-datadir "$OLDDATA" --new-datadir "$NEWDATA" \
+                  --old-bindir "$OLDBIN" --new-bindir "$NEWBIN" \
+                  --clone --jobs "$(nproc)" --no-sync -o '-F' -O '-F'
+
+                chmod = "$OLDDATA"
+              '';
+            };
+          };
         };
-      };
 
-      postgresql = ifCert {
-        wants = ["postgresql-certs.service"];
-        after = ["postgresql-certs.service"];
-      };
-
-      postgresql-upgrade = lib.mkIf (cfg.nextPackage != null && cfg.nextPackage.psqlSchema > config.services.postgresql.package.psqlSchema) {
-        conflicts = ["postgresql.service"];
-        wants = ["postgresql-certs.service"];
-        after = ["postgresql-certs.service"];
-        serviceConfig = {
-          Type = "oneshot";
-          RuntimeDirectory = "postgresql";
-          User = "postgres";
-          Group = "postgres";
-          ExecStart = pkgs.writeShellScript "postgresql-upgrade" ''
-            set -euxo pipefail
-
-            NEWDATA="/var/lib/postgresql/${cfg.nextPackage.psqlSchema}"
-            NEWBIN="${cfg.nextPackage}/bin"
-            OLDDATA="${config.services.postgresql.dataDir}"
-            OLDBIN="${config.services.postgresql.package}/bin"
-
-            rm -rf "$NEWDATA"
-
-            install -d -m 0700 -o postgres -g postgres "$NEWDATA"
-
-            cd "$NEWDATA"
-
-            "$NEWBIN/initdb" --no-sync --no-data-checksums -D "$NEWDATA"
-
-            "$NEWBIN/pg_upgrade" \
-              --old-datadir "$OLDDATA" --new-datadir "$NEWDATA" \
-              --old-bindir "$OLDBIN" --new-bindir "$NEWBIN" \
-              --clone --jobs "$(nproc)" --no-sync -o '-F' -O '-F'
-
-            chmod = "$OLDDATA"
-          '';
+        services.postgresql.settings = ifCert {
+          ssl = true;
+          ssl_cert_file = "/var/lib/postgresql/ssl/fullchain.pem";
+          ssl_key_file = "/var/lib/postgresql/ssl/key.pem";
         };
-      };
-    };
-
-    services.postgresql.settings = ifCert {
-      ssl = true;
-      ssl_cert_file = "/var/lib/postgresql/ssl/fullchain.pem";
-      ssl_key_file = "/var/lib/postgresql/ssl/key.pem";
-    };
-  };
+      }
+    ];
 }
