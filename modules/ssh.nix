2fa79c8ca82ffb20901822ae6ed4b87d4756698c
Albert Peschar <albert@peschar.net>
2026-06-19 12:02:55 +0000

    ssh: adapt AcceptEnv to option type

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -2,7 +2,26 @@
   lib,
   options,
   ...
-}: {
+}: let
+  acceptEnv = [
+    "GIT_AUTHOR_NAME"
+    "GIT_AUTHOR_EMAIL"
+    "GIT_COMMITTER_NAME"
+    "GIT_COMMITTER_EMAIL"
+  ];
+
+  settingsValueType = options.services.openssh.settings.type.nestedTypes.freeformType.nestedTypes.elemType or null;
+  settingsTypeDescription = options.services.openssh.settings.type.description or "";
+  # NixOS 26.05 is expected to accept lists for settings such as AcceptEnv.
+  settingsAcceptsLists =
+    if settingsValueType != null
+    then settingsValueType.check acceptEnv
+    else lib.hasInfix "list" settingsTypeDescription;
+  acceptEnvValue =
+    if settingsAcceptsLists
+    then acceptEnv
+    else lib.concatStringsSep " " acceptEnv;
+in {
   services.openssh =
     {
       enable = true;
@@ -11,12 +30,7 @@
       settings = {
         PasswordAuthentication = false;
         KbdInteractiveAuthentication = false;
-        AcceptEnv = [
-          "GIT_AUTHOR_NAME"
-          "GIT_AUTHOR_EMAIL"
-          "GIT_COMMITTER_NAME"
-          "GIT_COMMITTER_EMAIL"
-        ];
+        AcceptEnv = acceptEnvValue;
       };
     };
 
