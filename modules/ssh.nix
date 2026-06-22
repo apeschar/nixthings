a0d4cdafb1c70db8732c1f7b3327fa67dd9e230d
Albert Peschar <albert@peschar.net>
2026-06-22 05:18:20 +0000

    Revert "ssh: adapt AcceptEnv to option type"
    This reverts commit 2fa79c8ca82ffb20901822ae6ed4b87d4756698c.

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -2,26 +2,7 @@
   lib,
   options,
   ...
-}: let
-  acceptEnv = [
-    "GIT_AUTHOR_NAME"
-    "GIT_AUTHOR_EMAIL"
-    "GIT_COMMITTER_NAME"
-    "GIT_COMMITTER_EMAIL"
-  ];
-
-  settingsValueType = options.services.openssh.settings.type.nestedTypes.freeformType.nestedTypes.elemType or null;
-  settingsTypeDescription = options.services.openssh.settings.type.description or "";
-  # NixOS 26.05 is expected to accept lists for settings such as AcceptEnv.
-  settingsAcceptsLists =
-    if settingsValueType != null
-    then settingsValueType.check acceptEnv
-    else lib.hasInfix "list" settingsTypeDescription;
-  acceptEnvValue =
-    if settingsAcceptsLists
-    then acceptEnv
-    else lib.concatStringsSep " " acceptEnv;
-in {
+}: {
   services.openssh =
     {
       enable = true;
@@ -30,7 +11,12 @@ in {
       settings = {
         PasswordAuthentication = false;
         KbdInteractiveAuthentication = false;
-        AcceptEnv = acceptEnvValue;
+        AcceptEnv = [
+          "GIT_AUTHOR_NAME"
+          "GIT_AUTHOR_EMAIL"
+          "GIT_COMMITTER_NAME"
+          "GIT_COMMITTER_EMAIL"
+        ];
       };
     };
 
